98096eef9faa90f7ae9594c763e236bb30bf6464
Seth Troisi <sethtroisi@google.com>
2025-10-09 21:52:07 -0700

    Initial goldbach commit

---
+++ Makefile
@@ -0,0 +1,37 @@
+# Copyright 2025 Seth Troisi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+OPT     = -O3 -std=c++20 -g
+OBJS	=
+OUT	= goldbach
+CC	= g++
+CFLAGS	= $(OPT) -Wall -Werror -Wno-vla -fopenmp
+
+LDFLAGS	= -lgmp -lprimesieve
+# Need for local gmp / primesieve
+LDFLAGS+= -L /usr/local/lib
+
+
+all: $(OUT)
+
+%.o: %.cpp
+	$(CC) -c -o $@ $< $(CFLAGS) $(DEFINES)
+
+goldbach: goldbach.cpp $(OBJS)
+	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS) $(DEFINES)
+
+.PHONY: all clean
+
+clean:
+	rm -f $(OUT) *.o
---
+++ goldbach.cpp
@@ -0,0 +1,44 @@
+// Copyright 2025 Seth Troisi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <clocale>
+#include <cmath>
+#include <csignal>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <sstream>
+#include <thread>
+#include <type_traits>
+#include <vector>
+
+#include <gmp.h>
+#include <omp.h>
+#include <primesieve.hpp>
+
+using std::cout;
+using std::endl;
+using std::map;
+using std::mutex;
+using std::pair;
+using std::vector;
+using namespace std::chrono;
+
+int main(void) {
+}

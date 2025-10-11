bb97b88fc1c645bb43a549a73214ea2a8ab6c647
Seth Troisi <sethtroisi@google.com>
2025-10-10 22:36:10 -0700

    Improve checking of overflow candidates
    ```
    time ./goldbach -t1 -K 1024 -e 1''000''000''000
    real	0m0.573s
    user	0m0.569s
    sys	0m0.003s
    
    time ./goldbach -t1 -K 1024 -s 1''000''000''000''000''000 -e 1''000''001''000''000''000
    real	0m0.833s
    user	0m0.795s
    sys	0m0.038s
    
    time ./goldbach -t1 -K 1536 -s 1''000''000''000''000''000''000 -e 1''000''000''001''000''000''000
    real	0m2.814s
    user	0m2.476s
    sys	0m0.336s
    ```

--- goldbach.cpp
+++ goldbach.cpp
@@ -13,32 +13,38 @@
 // limitations under the License.
 
 #include <atomic>
-#include <bitset>
 #include <cassert>
 #include <chrono>
 #include <clocale>
 #include <cmath>
 #include <cstdio>
 #include <cstring>
-#include <iostream>
 #include <vector>
 #include <getopt.h>
 
-#include <omp.h>
 #include <primesieve.hpp>
 
+using std::array;
 using std::atomic;
-using std::cout;
-using std::endl;
-using std::bitset;
 using std::vector;
 using namespace std::chrono;
 
 void goldbach_search(const struct Config &config);
 void goldbach_search_single(const Config &config);
 
-const uint32_t THREAD_SPACING = 256'000;
-const uint32_t OVERLAP = 1536;
+constexpr uint32_t THREAD_SPACING = 256'000;
+constexpr uint32_t OVERLAP = 1024 + 512;
+
+// See https://oeis.org/A025019 and
+// "EMPIRICAL VERIFICATION OF THE EVEN GOLDBACH CONJECTURE" by TOMAS OLIVEIRA e SILVA
+constexpr uint32_t MAX_Q = 10'000;
+// must be greater than primepi(2 * THREAD_SPACING + MAX_Q)
+constexpr uint32_t PRIMES_SIZE = []() constexpr -> uint32_t {
+    uint32_t RANGE = 2 * THREAD_SPACING + MAX_Q;
+    uint32_t PRIME_PI = 1.1 * RANGE / log(RANGE);
+    return 1024 * ((PRIME_PI + 1024 - 1) / 1024);
+}();
+constexpr uint32_t MAX_PRIME_GAP = 772; // Good up to 10^20
 
 struct Config {
     // start
@@ -99,44 +105,25 @@ int main(int argc, char* argv[]) {
     // Display %'d with commas i.e. 12,345
     setlocale(LC_NUMERIC, "");
 
-
     Config config = argparse(argc, argv);
 
     goldbach_search(config);
 }
 
-atomic<uint16_t> max_needed_P(0);
-bool set_max(uint16_t P_i) {
+atomic<uint16_t> max_needed_Q(0);
+bool set_max(uint16_t q_i) {
     // Use atomic::fetch_max after c++26
 
-    uint16_t was = max_needed_P.load();
-    while (was < P_i) {
+    uint16_t was = max_needed_Q.load();
+    while (was < q_i) {
         // Try to store
-        if (max_needed_P.compare_exchange_strong(was, P_i)) {
+        if (max_needed_Q.compare_exchange_strong(was, q_i)) {
             return true;
         }
-        was = max_needed_P.load();
+        // Was updated in the middle, try again with new value from max_needed_Q
+        was = max_needed_Q.load();
     }
     return false;
-}
-
-int brute_check(uint64_t N, const vector<uint16_t> &P) {
-    primesieve::iterator it(N);
-    size_t P_i = 0;
-    uint64_t q = P[P_i];
-    for (uint64_t p = it.prev_prime(); P_i < P.size(); p = it.prev_prime()) {
-        while (p + q < N && (++P_i) < P.size()) {
-            q = P[P_i];
-        }
-        // Check if p + q = N
-        if (p + q == N) {
-            if (set_max(P_i)) {
-                printf("\t\t%lu = %lu + %lu (%lu)\n", N, p, q, P_i);
-            }
-            return P_i;
-        }
-    }
-    return -1;
 }
 
 const size_t MASK_BITS = 64;
@@ -201,15 +188,18 @@ void goldbach_search_single(const Config &config) {
 
     // See https://oeis.org/A025019 and
     // "EMPIRICAL VERIFICATION OF THE EVEN GOLDBACH CONJECTURE" by TOMAS OLIVEIRA e SILVA
-    vector<uint16_t> BRUTE_P;
-    primesieve::generate_primes(3, 10000, &BRUTE_P);
+    vector<uint16_t> Q;
+    primesieve::generate_primes(3, MAX_Q, &Q);
 
-    printf("Searching [%lu, %lu] P <= %u, spacing = %u (overlap: %u)\n",
-            config.n_0, config.n_1, config.K, THREAD_SPACING, OVERLAP);
+    const auto n_0 = config.n_0;
+    const auto n_1 = config.n_1;
+    printf("Searching [%lu, %lu] P <= %u, spacing = %u (overlap: %u, <= %u primes)\n",
+            n_0, n_1, config.K, THREAD_SPACING, OVERLAP, PRIMES_SIZE);
     printf("\t|P| = %lu (%lu masks)\n", P.size(), P_masks[31].size());
     printf("\n");
+    assert( n_1 >= n_0 );
     assert( (unsigned) config.K <= OVERLAP );
-    assert( config.n_1 >= config.n_0 );
+    assert( OVERLAP <= MAX_Q );
 
     assert( THREAD_SPACING % 64 == 0);
     assert( OVERLAP % 64 == 0);
@@ -222,20 +212,33 @@ void goldbach_search_single(const Config &config) {
     uint64_t *overlap = cleared + THREAD_UINT64_PER;
     uint64_t *cleared_after_overlap = cleared + (OVERLAP_BYTES / 8);
 
+    uint64_t prime_start = 0;
+    uint64_t prime_end = 0;
+
+    array<uint64_t, PRIMES_SIZE> circular_primes;
+
     size_t NON_OVERLAPPED_BYTES = THREAD_BYTES - OVERLAP_BYTES;
     uint64_t FULL_MASK = std::numeric_limits<uint64_t>::max();
 
     // Break up [n_0, n_1] into ranges of THREAD_SPACING
     uint64_t M = 2 * THREAD_SPACING;
-    uint64_t M_COUNT = (config.n_1 - config.n_0 + 1 + M-1) / M;
+    uint64_t M_COUNT = (n_1 - n_0 + 1 + M-1) / M;
     atomic<size_t> brute_searches = 0;
 
-    // Handle overflow from previous interval
-    if (config.n_0 > 0) {
-        uint64_t n_0 = config.n_0;
+    // Need primes up to MAX_Q prior to n_0
+    uint64_t min_needed_prime = std::max<int64_t>((int64_t)n_0 - MAX_Q - MAX_PRIME_GAP, int64_t(3));
+    assert( min_needed_prime >= 3);
+    primesieve::iterator it(min_needed_prime);
+
+    // Handle overflow from previous interval and primes needed for brute check
+    if (n_0 > 0) {
         assert( n_0 >= OVERLAP );
-        primesieve::iterator it(std::max(n_0 - OVERLAP, uint64_t(3)));
-        for (uint64_t prime = it.next_prime(); prime < n_0; prime = it.next_prime()) {
+        uint64_t prime;
+        for (prime = it.next_prime(); prime < n_0 - OVERLAP; prime = it.next_prime()) {
+            circular_primes[prime_end++] = prime;
+        }
+        for (; prime < n_0; prime = it.next_prime()) {
+            circular_primes[prime_end++] = prime;
             for (const uint32_t q : P) {
                 int64_t i = prime + q - n_0;
                 if (i >= 0) {
@@ -246,19 +249,38 @@ void goldbach_search_single(const Config &config) {
                 }
             }
         }
+        assert(prime_end <= PRIMES_SIZE);
     }
 
-    primesieve::iterator it(std::max(config.n_0, uint64_t(3)));
     uint64_t prime = it.next_prime();
     for (uint64_t m_i = 0 ; m_i < M_COUNT; m_i ++) {
-        uint64_t M_0 = config.n_0 + M * m_i;
+        uint64_t M_0 = n_0 + M * m_i;
         uint64_t M_1 = M_0 + M-1;
         // [M_0, M_1]
 
+        // Handle updating prime_start to last prime <= M_0 - MAX_Q
+        if (m_i > 0) {
+            min_needed_prime = std::max<int64_t>((int64_t) M_0 - MAX_Q, int64_t(3));
+            //printf("\tAdvancing up to %lu, prime_start[%lu] = %lu to ", min_needed_prime, prime_start, circular_primes[prime_start]);
+            uint32_t next_i = prime_start;
+            while (circular_primes[next_i] <= min_needed_prime) {
+                assert( prime_start != prime_end ); // Can't advance past prime_end
+                prime_start = next_i;
+                next_i = next_i == (PRIMES_SIZE - 1) ? 0 : next_i + 1;
+            }
+            //printf("[%lu] = %lu\n", prime_start, circular_primes[prime_start]);
+            assert( circular_primes[prime_start] <= min_needed_prime );
+            assert( circular_primes[prime_start] + MAX_PRIME_GAP > min_needed_prime );
+            assert( circular_primes[next_i] > min_needed_prime );
+        }
+
         // Should/Could check prev_prime
-        assert(prime >= M_0 && prime < M_0 + 772);
+        assert(M_0 <= prime && prime < M_0 + MAX_PRIME_GAP);
 
         for (; prime <= M_1; prime = it.next_prime()) {
+            circular_primes[prime_end++] = prime;
+            if (prime_end == PRIMES_SIZE) prime_end = 0;
+
             uint64_t i = prime - M_0;
             assert( i <= M );
             if (false) {
@@ -298,33 +320,97 @@ void goldbach_search_single(const Config &config) {
         }
 
         // Verify all cleared
-        for (size_t i = 0; i < THREAD_UINT64_PER; i++) {
-            uint64_t bits = cleared[i];
-            if (bits != FULL_MASK) {
-                for (size_t j = 0; j < MASK_BITS; j++) {
-                    if (!(bits & (1UL << j))) {
-                        uint64_t t = M_0 + 2*(MASK_BITS*i+j);
-                        brute_searches++;
-                        if (t > 4) {
-                            int q_i = brute_check(t, BRUTE_P);
-                            // Need to ignore
-                            if (q_i < 0 || q_i < (signed) P.size() ) {
-                                printf("\t%lu not found [%lu, %lu) = %lx\n",
-                                        t, M_0 + 2*MASK_BITS*i, M_0 + 2*MASK_BITS*(i+1), bits);
+        {
+            // Add final prime at prime_end without increment.
+            circular_primes[prime_end] = prime;
+
+            //printf("Verifying [%lu, %lu] primes(indexes %lu %lu) [%lu, %lu]\n",
+            //       M_0, M_1, prime_start, prime_end, circular_primes[prime_start], circular_primes[prime_end]);
+
+            assert( (circular_primes[prime_start] == 3) || (circular_primes[prime_start] + MAX_Q < M_0) );
+            assert( circular_primes[prime_end] >= M_1 );
+
+            // Search forward till prime just less than M_0.
+            uint32_t prime_i = prime_start;
+            uint32_t next_i = prime_start;
+            while (circular_primes[next_i] <= M_0) {
+                assert( prime_i != prime_end ); // Can't advance past prime_end
+                prime_i = next_i;
+                next_i = next_i == (PRIMES_SIZE - 1) ? 0 : next_i + 1;
+            }
+
+            for (size_t i = 0; i < THREAD_UINT64_PER; i++) {
+                uint64_t bits = cleared[i];
+                if (bits != FULL_MASK) {
+                    for (size_t j = 0; j < MASK_BITS; j++) {
+                        if (!(bits & (1UL << j))) {
+                            brute_searches++;
+                            uint64_t t = M_0 + 2*(MASK_BITS*i+j);
+                            if (t > 4) {
+                                // Could be (t - K) but that verifies less
+                                while (circular_primes[prime_i] < t) {
+                                    assert( prime_i != prime_end ); // Can't advance past prime_end
+                                    prime_i = next_i;
+                                    next_i = next_i == (PRIMES_SIZE - 1) ? 0 : next_i + 1;
+                                }
+
+                                size_t q_i;
+                                {
+                                    // Walk backwards in circular_primes and forwards in Q
+                                    uint64_t p_i = prime_i;
+                                    uint64_t p = circular_primes[p_i];
+                                    uint64_t p_i_0 = prime_i;
+                                    uint64_t p_0 = p;
+                                    assert( p >= t );
+
+                                    bool valid = false;
+                                    q_i = 0;
+                                    for (uint64_t q = Q[q_i]; q_i < Q.size(); q = Q[++q_i]) {
+                                        while (p + q > t) {
+                                            // Can't advance backwards to less than prime_start
+                                            assert( p_i != prime_start );
+                                            p_i = (p_i == 0) ? (PRIMES_SIZE - 1) : (p_i - 1);
+                                            p = circular_primes[p_i];
+                                        }
+                                        // Check if p + q = N = t
+                                        if (p + q == t) {
+                                            if (set_max(q_i)) {
+                                                printf("\t\t%lu = %lu + %lu (%lu)\n", t, p, q, q_i);
+                                            }
+                                            valid = true;
+                                            break;
+                                        }
+                                    }
+                                    if (!valid) {
+                                        printf("Didn't find p + q = %lu (searched p = %lu to %lu (indexes %lu to %lu), q = %u to %u)\n",
+                                                t, p_0, p, p_i_0, p_i, Q.front(), Q.back());
+                                        assert( false );
+                                    }
+                                }
+
+                                if (q_i < P.size() ) {
+                                    printf("\t%lu not found [%lu, %lu) = %lx\n",
+                                            t, M_0 + 2*MASK_BITS*i, M_0 + 2*MASK_BITS*(i+1), bits);
+                                    assert( false );
+                                }
+                                assert( Q[q_i] > config.K );
                             }
-                            assert( q_i >= 0 && q_i >= (signed) P.size() );
                         }
                     }
                 }
             }
         }
 
+
         // Move bits down and clear stuff out
         memcpy(cleared, overlap, OVERLAP_BYTES);
         memset(cleared_after_overlap, 0, NON_OVERLAPPED_BYTES);
 
-        if (m_i < 10 || ((m_i & 511) == 0) || (m_i + 5 > M_COUNT)) {
-            printf("\t[%lu, %lu] (brute: %lu)\n", M_0, M_1, brute_searches.load());
+        if (m_i < 10 || (m_i + 5 > M_COUNT)
+                || (m_i < 1024 && ((m_i & 127) == 0))
+                || ((m_i & 2047) == 0)) {
+            printf("\t[%lu, %lu] (%.1f%%) (brute: %lu)\n",
+                   M_0, M_1, 100.0 * m_i / M_COUNT, brute_searches.load());
         }
     }
 }
